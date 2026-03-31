23fdf22f078cbb4a4c691b6a0932f4486b80f12c
Dominic Roche <69010816+DominicRocheCMS@users.noreply.github.com>
2026-03-31 12:58:59 -0400

    Restore Windows WebView default launch

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-import "./src/main-new.js";
+const forceBrowserMode = process.env.PYTHIA_BROWSER_MODE === '1';
+
+if (process.platform === 'win32' && !forceBrowserMode) {
+	await import('./src/main.js');
+} else {
+	await import('./src/main-new.js');
+}
